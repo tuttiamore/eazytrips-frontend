62c785ce5bfa7349f15ad9e866983f1b1d33bee7
ThomasBeckDresden <thomas.beck.dresden@gmail.com>
2021-07-22 17:47:05 +0200

    adjust to figma template

--- src/styles/useCardStyle.js
+++ src/styles/useCardStyle.js
@@ -3,12 +3,15 @@ import { makeStyles } from "@material-ui/core/styles";
 const useCardStyle = makeStyles((theme) => ({
   root: {
     maxWidth: "100%",
-
+    padding: "8px",
     // margin: " 16px auto",
   },
   media: {
     height: 0,
-    paddingTop: "", // 16:9
+    // 16:9
+  },
+  tick: {
+    padding: "0",
   },
   expand: {
     transform: "rotate(0deg)",
@@ -16,12 +19,40 @@ const useCardStyle = makeStyles((theme) => ({
     transition: theme.transitions.create("transform", {
       duration: theme.transitions.duration.shortest,
     }),
+    padding: "8px",
   },
   expandOpen: {
     transform: "rotate(180deg)",
   },
   avatar: {
     backgroundColor: theme.palette.primary.main,
+  },
+  outer: {
+    margin: "8px 0",
+  },
+  div: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    padding: "8px",
+  },
+  span: {
+    backgroundColor: theme.palette.secondary.light,
+    width: "8px",
+    height: "80px",
+    margin: "0 16px",
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+    borderRadius: "16px",
+  },
+  distance: {
+    padding: "4px",
+  },
+  large: {
+    width: theme.spacing(7),
+    height: theme.spacing(7),
   },
 }));
 
--- src/views/TripSingleDay.js
+++ src/views/TripSingleDay.js
@@ -1,24 +1,48 @@
 import React from "react";
-import List from "@material-ui/core/List";
+import { useParams } from "react-router-dom";
+import { List, ListItem } from "@material-ui/core/";
 import Card from "../components/Card";
 import mockData from "../mock.json";
 import NavStepper from "../components/Stepper";
+import useCardStyle from "../styles/useCardStyle";
+import DirectionsWalkIcon from "@material-ui/icons/DirectionsWalk";
+import Typography from "@material-ui/core/Typography";
 
 export default function TripSingleDay() {
   const tripData = mockData;
-  //console.log(tripData);
-
+  const { day } = useParams();
+  console.log(day);
+  const classes = useCardStyle();
   return (
     <>
       <NavStepper></NavStepper>
 
-      <List style={{ height: "100%", overflow: "auto" }}>
-        {tripData.trip[0].locations.map((element) => (
-          <Card
-            key={element.locationIndex}
-            type="TripSingleDay"
-            data={element}
-          />
+      <List
+        style={{
+          height: "100%",
+          overflow: "auto",
+          padding: "8px",
+          width: "80%",
+          margin: "auto",
+        }}
+      >
+        {tripData.trip[day - 1].locations.map((element, index) => (
+          <>
+            <Card
+              key={element.locationIndex}
+              type="TripSingleDay"
+              data={element}
+            />
+            {tripData.trip[day - 1].locations[index + 1] && (
+              <div className={classes.div}>
+                <span className={classes.span} />
+                <Typography className={classes.distance} variant="body1">
+                  {tripData.trip[day - 1].locations[index].travelTo} min
+                </Typography>
+                <DirectionsWalkIcon fontSize="large" />
+              </div>
+            )}
+          </>
         ))}
       </List>
     </>

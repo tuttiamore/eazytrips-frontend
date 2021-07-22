10c0a0bbfb80e078ad3a191dad26bf6145d891cf
ThomasBeckDresden <thomas.beck.dresden@gmail.com>
2021-07-22 10:20:36 +0200

    card ready

--- src/views/TripSingleDay.js
+++ src/views/TripSingleDay.js
@@ -1,5 +1,23 @@
 import React from "react";
+import List from "@material-ui/core/List";
+import Card from "../components/Card";
+import mockData from "../mock.json";
 
 export default function TripSingleDay() {
-  return <div>trip single day</div>;
+  const tripData = mockData;
+  //console.log(tripData);
+
+  return (
+    <>
+      <List style={{ height: "100%", overflow: "auto" }}>
+        {tripData.trip[0].locations.map((element) => (
+          <Card
+            key={element.locationIndex}
+            type="TripSingleDay"
+            data={element}
+          />
+        ))}
+      </List>
+    </>
+  );
 }
--- views/TripSingleDay.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function TripSingleDay() {
-  return <div>trip single day</div>;
-}

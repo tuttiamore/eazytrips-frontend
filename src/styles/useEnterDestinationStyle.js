150433492945e3e8053c3dfc4985b093510f807a
Jugesh <jugeshkumar0208@gmail.com>
2021-07-27 17:59:06 +0200

    finished the enter destination page

---
+++ src/styles/useEnterDestinationStyle.js
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useEnterDestinationStyle = makeStyles((theme) => ({
+    chipAlignSelf: {
+        alignSelf: "center",
+    },
+})
+)
+
+export default useEnterDestinationStyle;

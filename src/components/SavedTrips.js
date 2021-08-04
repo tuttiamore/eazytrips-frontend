9d2e9fcc023c045d8983750a734bf82e5782aefb
ThomasBeckDresden <thomas.beck.dresden@gmail.com>
2021-08-04 14:39:44 +0200

    implement refresh

--- src/components/SavedTrips.js
+++ src/components/SavedTrips.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { useTripContext } from "../context/TripContext";
+import { get_user_trips } from "../auth/auth";
+import CachedIcon from "@material-ui/icons/Cached";
 
 export default function SavedTrips({ me }) {
   const { savedTrips } = useTripContext();
+  const handleRefreshClick = async () => await get_user_trips();
   console.log(me);
   console.log(savedTrips);
-  return <div>bla</div>;
+  return <CachedIcon color="primary" onClick={handleRefreshClick} />;
 }
